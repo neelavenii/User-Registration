c29577fb869aed8773faf9932b56a674740cf37b
eswar <neelaveni.eswar04@gmail.com>
2022-04-26 06:01:08 +0530

    [Eswar]Validating FirstNmae

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UserRegistration
@@ -11,6 +12,15 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration");
+
+            //object created
+            UserRegistrationValidation validate = new UserRegistrationValidation();
+            Console.WriteLine("Enter First Name:");
+            string firstName = Console.ReadLine();
+            bool fNameResult = validate.ValidateFirstName(firstName);
+            validate.PrintResult(fNameResult);
+
+            Console.WriteLine("--------------");
             Console.ReadLine();
         }
     }
---
+++ UserRegistration/UserRegistrationValidation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+     class UserRegistrationValidation
+    {
+        private static string Regex_FIRSTNAME = "^[A-Z][a-z]{2}$";
+        //method to set Firstname
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, Regex_FIRSTNAME);
+
+        }
+        //to print result
+        public void PrintResult(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("Valid.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid.");
+            }
+        }
+    }
+}
